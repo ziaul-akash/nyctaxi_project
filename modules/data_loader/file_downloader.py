b5a33bb3462dbc580d038068284bbc4078afed92
ziaul-akash <iam.akash966@gmail.com>
2026-01-28 16:43:22 +0000

    changed dir_path

--- modules/data_loader/file_downloader.py
+++ modules/data_loader/file_downloader.py
@@ -2,12 +2,18 @@ import urllib.request
 import shutil
 import os
 
+
 def download_file(url: str, dir_path: str, local_path: str):
 
     """
     Downloads a file from the given URL to the local path
     """
-    os.makedirs(os.path.dirname(dir_path), exist_ok=True)
+    os.makedirs(dir_path, exist_ok=True)
 
     with urllib.request.urlopen(url) as response, open(local_path, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
+
+
+
+
+
